a25fa3d36b7004d30c01f759c700ef17f4f60854
Gigleydson Dias <gigleydson@gmail.com>
2024-05-11 13:00:26 -0300

    Retirada do max_lenght do campo quantidade

--- recibo_e_cautela/models.py
+++ recibo_e_cautela/models.py
@@ -12,18 +12,19 @@ class Base(models.Model):
 
 
 class Recibo(Base):
-    nome = models.CharField('Nome', max_length=100)
-    saram = models.CharField('Saram', max_length=7)
-    graduacao = models.CharField('Posto / Graduação', max_length=100)
-    setor = models.CharField('Setor / Seção', max_length=100)
-    ramal = models.CharField('Ramal', max_length=4)
+    nome = models.CharField('Nome', max_length = 100)
+    saram = models.CharField('Saram', max_length = 7)
+    graduacao = models.CharField('Posto / Graduação', max_length = 100)
+    setor = models.CharField('Setor / Seção', max_length = 100)
+    ramal = models.CharField('Ramal', max_length = 4)
     data = models.DateField('Data', default=datetime.date.today())
-    especificacoes = models.CharField('Especifiações', max_length=200)
-    observacao = models.CharField('Observação', max_length=500)
-    sinf = models.CharField('SINF', max_length=4)
-    lacre = models.CharField('Lacre', max_length=7)
-    bmp = models.CharField('BMP', max_length=10)
-    quantidade = models.IntegerField('Quantidade', max_length=100)
+    especificacoes = models.CharField('Especifiações', max_length = 200)
+    observacao = models.CharField('Observação', max_length = 500)
+    sinf = models.CharField('SINF', max_length = 4)
+    lacre = models.CharField('Lacre', max_length = 7)
+    bmp = models.CharField('BMP', max_length = 10)
+    # quantidade = models.IntegerField('Quantidade', max_length=100)
+    quantidade = models.IntegerField('Quantidade')
 
     def __str__(self):
         return self.nome
